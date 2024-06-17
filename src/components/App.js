dced88f6487d1437cd3200d658b1157730bab1e9
alohasally <peacheeks@gmail.com>
2024-06-17 22:12:36 +0900

    1st

---
+++ src/components/App.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  const handleToggle = (id) => {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...items, packed: !item.packed } : item
+      )
+    );
+  };
+  const handleCleanList = () => {
+    setItems([]);
+  };
+
+  const handleDelete = (id) => {
+    setItems((items) => items.filter((item) => item.id !== id));
+  };
+  return (
+    <div className="app">
+      <Logo />
+      <Form items={items} setItems={setItems} />
+      <PackingList
+        items={items}
+        onToggle={handleToggle}
+        onClean={handleCleanList}
+        onDelete={handleDelete}
+      />
+      <Stats />
+    </div>
+  );
+}
---
+++ src/components/Form.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+export default function Form({ items, setItems }) {
+  const [number, setNumber] = useState(1);
+  const [description, setDescription] = useState("");
+
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    const newItem = { number, description, packed: false, id: Date.now() };
+    setItems([...items, newItem]);
+
+    setNumber(1);
+    setDescription("");
+  };
+  console.log(items);
+  return (
+    <div className="add-form">
+      <h3>What do you need for your I trip?</h3>
+      <form onSubmit={(e) => handleSumbit(e)}>
+        <select
+          value={number}
+          onChange={(e) => setNumber(Number(e.target.value))}
+        >
+          {Array.from({ length: 20 }, (_, i) => (
+            <option>{i + 1}</option>
+          ))}
+        </select>
+        <input
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        ></input>
+      </form>
+      <button>Add</button>
+    </div>
+  );
+}
